f7623f56f0c4795b3f171bf0e97d08420d72c0fb
matheussouzalima <matholaslima@gmail.com>
2022-11-07 12:04:03 -0300

    Finalização rota post sign-up

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 import express from 'express'
 
 const app = express();
+app.use(express.json())
+
+let users = [];
+let tweets = [];
+let tweetsCompose = [];
 
 app.post("/sign-up",(req,res)=>{
-    console.log(req.body);
+    let user = {
+        username: req.body.username,
+        avatar: req.body.avatar
+    }
+    users.push(user);
+    res.status(201);
     res.send('OK');
 })
+
+app.post("/tweets",(req,res)=>{
+    let tweet = {
+        : ,
+        : 
+    }
+    users.push(user);
+    res.status(201);
+    res.send('OK');
+})
+
+
+app.listen(5000);
